a5a29f59b5c34d5344af52d1cf9f19f47609e744
tim <timothy.bywater@kswo.ch>
2020-08-08 07:27:49 -0600

    Glasgow fetch

--- Glasgow_Fetch.py
+++ Glasgow_Fetch.py
@@ -1,4 +1,20 @@
-import requests
+import urllib.request
+import csv
+
+def Glassgow_Fetch(DatenM):
+    ocol:csv'
+
+    for i in DatenM:
+        Month = i[0]
+        Day = i[1]
+        Hour = i[2]
+
+        if Hour = 24:
+
+
+
+
+        url = 'https://services.marinetraffic.com/api/exportvesseltrack/b07448715b200b24ccc79909b6cf721ba0f55d3d/'+'v:2/period:hourly/fromdate:2020-'+Month+'-'+Day' '+Hour+':00:00/todate:2020-'+Monthadd+'-'+Dayadd+' '+Houradd+':00:00/mmsi: 269266000 / prot
 
 
 
