199a7c34d3b4a0b803a2ba89f05c40c1d6fa7e06
fabianlachmann <fabian.lachmann2@gmail.com>
2020-08-12 18:15:58 +0200

    hello

--- MergeScript.py
+++ MergeScript.py
@@ -1,10 +1,17 @@
 import csv
 
 def Merge(Inputfiles, Outputfile):#Inputfiles als Array von str mit filelocations; Ouputfile als str mit directory
-    with open(Outputfile) as output:#öffnet das csv-file
-        csv_reader = csv.reader(output, delimiter=',') #initialisiert den Reader fürs csv
+    with open(Outputfile, mode='a', newline='') as output:  # a steht für append
+        csv_writer = csv.writer(output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 
         for input in Inputfiles:
             with open(input) as input:  # öffnet das csv-file
                 csv_reader = csv.reader(input, delimiter=',')  # initialisiert den Reader fürs csv
+                for row in csv_reader:
+                    csv_writer.writerow(row)
+
+
+
+    return
+
