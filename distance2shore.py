18cffb30f694e16b4f3610ac53a6dedc09b42f7d
tim <timothy.bywater@kswo.ch>
2020-08-25 12:23:37 +0200

    Why no work?

--- distance2shore.py
+++ distance2shore.py
@@ -1,5 +1,19 @@
 import requests
+#IDK why tf no works
 
+lat = "56.95415"
+lon = "-7.492229"
+key = "b19d3b66-0ac1-4ccc-b1dc-d8ba54be4610"
 
-def distance2shore():
-#da
+url1 = "https://api.kbgeo.com/coastal-distance/v2/coord?lat=<"+lat+">&lng=<"+lon+">"
+
+url = "https://api.kbgeo.com/coastal-distance/v2/coord?lat=<56.954153>&lng=<-7.492229>"
+
+headers1 = {"kb-auth-token: "+key}
+
+headers = {"kb-auth-token: b19d3b66-0ac1-4ccc-b1dc-d8ba54be4610"}
+r = requests.get(url, auth=headers)
+
+response = r.json()
+
+print(response)
